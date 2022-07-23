595f8894177d7c5c200efc821fa5c0020aa48944
pypygeek <pypygeek@gmail.com>
2022-07-23 15:08:28 +0000

    add about_me and main page

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('personal_page.urls')),
+    path('', include('single_pages.urls')),
 ]
