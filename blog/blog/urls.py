fbf03fa4658bf20a4992a789a5ed0272631c884d
pypygeek <pypygeek@gmail.com>
2022-07-23 14:42:38 +0000

    Add Detail Page

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog', include('personal_page.urls')),
+    path('blog/', include('personal_page.urls')),
 ]
