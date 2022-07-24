993411e1fa3cdb871d3daceea8f9d863edb6037b
pypygeek <pypygeek@gmail.com>
2022-07-24 06:45:28 +0000

    Add CBV Post Detail page

--- blog/personal_page/views.py
+++ blog/personal_page/views.py
@@ -1,17 +1,26 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/post_list.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')    
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {
+#            'posts' : posts,
+#        }
+#    )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
